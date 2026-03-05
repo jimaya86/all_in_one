bea68b9e0052d2ab7d68aac6fd89ff3f923a8d50
jima <jimaya@kakao.com>
2026-03-05 12:19:51 +0900

    연동 테스트

--- main.py
+++ main.py
@@ -588,8 +588,8 @@ def main(page: ft.Page):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port)
-
+    # assets_dir="assets" 추가
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port, assets_dir="assets")
 
 
 
